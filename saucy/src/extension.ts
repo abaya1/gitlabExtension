a14ac541af6154e77a238b8264b58c73e7c1c1c5
Arron Abay <arron.abay@creditone.com>
2024-07-11 13:27:11 -0700

    put everything in a start saucy command

--- saucy/src/extension.ts
+++ saucy/src/extension.ts
@@ -11,32 +11,19 @@ export const activate = async (context: vscode.ExtensionContext): Promise<void> 
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "saucy" is now active!');
 
-	// The command has been defined in the package.json file
+	// The command has been defined in the package.json file5
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('saucy.helloWorld', () => {
+	const main = vscode.commands.registerCommand('saucy.startSaucy', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Tariq!');
-	});
-
-	const getDateToast = vscode.commands.registerCommand('saucy.getDateToast', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const date = Date();
-
-		vscode.window.showInformationMessage(`The date is ${date}`);
-	});
-
-	const getGit = vscode.commands.registerCommand('saucy.getGit', async () => {
 		const gitController = new GitService();
 		await gitController.init();
 		await gitController.getRepositoryInfo();
 		vscode.window.showInformationMessage(`The current branch is ${gitController.currentBranch}`);
 	});
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(getDateToast);
+	context.subscriptions.push(main);
 };
 
 // This method is called when your extension is deactivated
