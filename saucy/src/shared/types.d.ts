178b29dc9bd7597798c63b3c55f1e3a0db04e2fa
Juan Echevarria <jechevarria00@gmail.com>
2024-07-15 13:52:38 -0700

    typings

---
+++ saucy/src/shared/types.d.ts
@@ -0,0 +1,24 @@
+interface PositionType {
+    base_sha: string;
+    start_sha: string;
+    head_sha: string;
+    old_path: string;
+    new_path: string;
+    position_type: string;
+    old_line?: number;
+    new_line: number;
+    line_range: {
+        start: {
+            line_code: string;
+            type: string;
+            old_line?: number;
+            new_line: number;
+        };
+        end: {
+            line_code: string;
+            type: string;
+            old_line?: number;
+            new_line: number;
+        };
+    };
+}
